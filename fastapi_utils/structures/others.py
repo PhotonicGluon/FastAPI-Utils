092dcd22bfb64f9994a67819d6d2e6b0e05225cb
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-12-13 15:32:55 +0800

    Add simple throw method for `ErrorDetail`

--- fastapi_utils/structures/others.py
+++ fastapi_utils/structures/others.py
@@ -8,3 +8,8 @@ class ErrorDetail(BaseModel):
     @classmethod
     def from_exception(cls, exception: Exception) -> "ErrorDetail":
         return cls(type=type(exception).__name__, message=str(exception))
+
+    @classmethod
+    def throw(cls, serialized: dict):
+        detail = cls(**serialized)
+        raise eval(detail.type)(detail.message)
