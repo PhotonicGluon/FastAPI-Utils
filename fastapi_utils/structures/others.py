78a2b55d00254eb813cc647f713978a166b4c997
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-02-17 14:14:51 +0800

    Add docstrings for `ErrorDetail`

--- fastapi_utils/structures/others.py
+++ fastapi_utils/structures/others.py
@@ -2,14 +2,35 @@ from pydantic import BaseModel
 
 
 class ErrorDetail(BaseModel):
+    """
+    Class that encapsulates a serializable error for passing into a response.
+    """
+
     type: str
+    "Error class/type"
+
     message: str
+    "Error message"
 
     @classmethod
     def from_exception(cls, exception: Exception) -> "ErrorDetail":
+        """
+        Creates an error detail object from an exception.
+
+        :param exception: exception to use
+        :return: created `ErrorDetail` object
+        """
+
         return cls(type=type(exception).__name__, message=str(exception))
 
     def throw(self):
+        """
+        Throws an error using the details in this object
+
+        :raises eval: evaluated error
+        :raises RuntimeError: if the error type is not one of the built-in types
+        """
+
         try:
             raise eval(self.type)(self.message)
         except NameError:
@@ -17,5 +38,9 @@ class ErrorDetail(BaseModel):
 
     @classmethod
     def throw_from_serialized(cls, serialized: dict):
+        """
+        Throws an error from a serialized `ErrorDetail` object.
+        """
+
         detail = cls(**serialized)
         detail.throw()
